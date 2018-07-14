067156912c2b8aec862e0fef570f265bd04ee6dc
Projects <projects@Nates-MacBook-Pro.local>
2018-07-14 14:46:05 -0700

    Made a bunch of changes. Separated functions into a utils file, still not best organization. Wrote classes to handle alters and reactions. Decided that there should be a single post that the bot will edit with alerts.

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,72 +1,56 @@
 import random
 import asyncio
 import aiohttp
-import json, os
+import json
+import os
 import discord
 from discord import Game
 from discord.ext.commands import Bot
-from discord.utils import find
+
+from utils import (get_channel_message, get_original_content,
+                   add_user_to_content, remove_user_from_content, is_alert_channel, ReservedMessage, RECOGNIZED_EMOJIS)
 
 BOT_PREFIX = ("?", "!")
 TOKEN = os.environ['BOT_TOKEN']
 CHANNEL_NAME = os.environ['BOT_REPORT_CHANNEL_NAME']
 
+
 client = Bot(command_prefix=BOT_PREFIX)
 
-RECOGNIZED_EMOJIS = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟', '❓', '❔']
 
-
-@client.command(name='8ball',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['eight_ball', 'eightball', '8-ball'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'That is a resounding no',
-        'It is not looking likely',
-        'Too hard to tell',
-        'It is quite possible',
-        'Definitely',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
-
-
-def get_channel_message(client, channel, content):
-    #  returns the first message with the given content in the given channel.
-    existing_message = find(lambda m: m.content==content and m.channel==channel, client.messages)
-    return existing_message
-
-
-@client.command()
-async def square(number):
-    squared_value = int(number) * int(number)
-    await client.say(str(number) + " squared is " + str(squared_value))
+# Keys are message ids, values are messages
+alerts = {}
+reserved_messages = {}
 
 
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="with humans"))
     print("Logged in as " + client.user.name)
+    all_alert_channels = [
+        channel for channel in client.get_all_channels() if is_alert_channel(channel)]
+    for channel in all_alert_channels:
+        message = await client.send_message(channel, '*Reserved*')
+        reserved_message = ReservedMessage(message)
+        reserved_messages[channel] = reserved_message
+
 
 @client.event
 async def on_reaction_add(reaction, user):
-    reacted_message = reaction.message
-    reporting_channel = discord.utils.get(reacted_message.server.channels, name=CHANNEL_NAME)
-    existing_message = get_channel_message(client, reporting_channel, reacted_message.content)
-    if not existing_message and reaction.emoji in RECOGNIZED_EMOJIS:
-        await client.send_message(reporting_channel, reacted_message.content)
+    if is_alert_channel(reaction.message.channel) and reaction.emoji in RECOGNIZED_EMOJIS:
+        reserved_message = reserved_messages[reaction.message.channel]
+        alert = reserved_message.get_or_create_alert(reaction.message)
+        alert.responses.post(reaction, user)
+        reserved_message.compose_content()
+        message = await client.edit_message(reserved_message.message, reserved_message.content)
+        await client.pin_message(message)
 
 
 @client.event
 async def on_reaction_remove(reaction, user):
-    reacted_message = reaction.message
-    if not reacted_message.reactions:
-        #  remove message from reporting channel
-        reporting_channel = discord.utils.get(reacted_message.server.channels, name=CHANNEL_NAME)
-        existing_message = get_channel_message(client, reporting_channel, reacted_message.content)
-        if existing_message:
-            await client.delete_message(existing_message)
+    if is_alert_channel(reaction.message.channel) and reaction.emoji in RECOGNIZED_EMOJIS:
+        content = remove_user_from_content(reaction.message, reaction, user)
+        await client.edit_message(reaction.message, content)
 
 
 async def list_servers():
---
+++ discord_bot/utils.py
@@ -0,0 +1,108 @@
+
+from collections import defaultdict
+from discord.utils import find
+
+RECOGNIZED_EMOJIS = ['1⃣', '2⃣', '3⃣', '4⃣',
+                     '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟', '❓', '❔']
+EMOJI_TEXT = {'1⃣': '1', '2⃣': '2', '3⃣': '3', '4⃣': '4', '5⃣': '5',
+              '6⃣': '6', '7⃣': '7', '8⃣': '8', '9⃣': '9', '🔟': '10', '❓': '?', '❔': '?'}
+
+
+class ReservedMessage():
+    def __init__(self, message):
+        self.message = message
+        self.content = message.content
+        self.alerts = {}
+
+    def get_alert(self, message):
+        try:
+            alert = self.alerts[message.id]
+        except KeyError:
+            alert = None
+        finally:
+            return alert
+
+    def get_or_create_alert(self, message):
+        alert = self.get_alert(message)
+        if not alert:
+            alert = Alert(message.content)
+            self.alerts[message.id] = alert
+        return alert
+
+    def compose_content(self):
+        content = ''
+        for message_id, alert in self.alerts.items():
+            print(f'before {alert.original_content}\n{alert.content}')
+            alert.compose_content()
+            print(f'after {alert.original_content}\n{alert.content}')
+            content += f'\n{alert.content}'
+        if not content:
+            content = '*Reserved*'
+        self.content = content
+
+
+class Alert():
+    def __init__(self, content):
+        self.responses = Responses()
+        self.original_content = content
+        self.content = content
+
+    def edit_content(self, content):
+        self.content = content
+
+    def edit_original_content(self, content):
+        self.original_content = content
+
+    def compose_content(self):
+        content = self.original_content
+        content += '\nRSVPs:'
+        for emoji, users in self.responses.items():
+            for user in users:
+                content += f' {user.name} ({EMOJI_TEXT[emoji]})'
+        self.content = content
+
+
+class Responses(defaultdict):
+    def __init__(self):
+        defaultdict.__init__(self, list)
+
+    def post(self, reaction, user):
+        self[reaction.emoji].append(user)
+
+    def delete(self, reaction, user):
+        self[reaction.emoji] = [
+            responded_user for responded_user in self[reaction.emoji] if responded_user != user]
+
+
+def is_alert_channel(channel):
+    return 'alert' in channel.name.lower()
+
+
+def get_original_content(content):
+    return content.split('RSVPs:')[0]
+
+
+def get_channel_message(client, channel, content):
+    #  returns the first message with the given content in the given channel.
+    original_content = get_original_content(content)
+    existing_message = find(lambda m: get_original_content(
+        m.content) == original_content and m.channel == channel, client.messages)
+    return existing_message
+
+
+def add_user_to_content(message, reaction, user):
+    # adds names to message content
+    print(message.content)
+    message_content = message.content.split('RSVPs:')[0]
+    try:
+        current_rsvps = message.content.split('RSVPs:')[1]
+    except IndexError:
+        updated_rsvps = f"{user.name} {reaction.emoji}"
+    else:
+        updated_rsvps = f"{current_rsvps}, {user.name} {reaction.emoji}"
+    return f'{message_content} RSVPs: {updated_rsvps}'
+
+
+def remove_user_from_content(message, reaction, user):
+    # remove user from message
+    return message.content
