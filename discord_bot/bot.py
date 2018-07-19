cae2a7d4e1bbb1cf1614b166a1be0be79cd9ccab
Projects <projects@Nates-MacBook-Pro.local>
2018-07-18 18:16:15 -0700

    Changed commands to be stand alone instead of defined in on_message.

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -10,7 +10,7 @@ from discord.ext.commands import Bot
 
 from utils import (is_alert_channel, RSVPMessage, RECOGNIZED_EMOJIS)
 
-BOT_PREFIX = ("?", "!")
+BOT_PREFIX = ("!")
 TOKEN = os.environ['BOT_TOKEN']
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -22,34 +22,28 @@ rsvp_messages = {}
 async def on_ready():
     await client.change_presence(game=Game(name="with humans"))
     print("Logged in as " + client.user.name)
-    # all_alert_channels = [
-    #     channel for channel in client.get_all_channels() if is_alert_channel(channel)]
-    # for channel in all_alert_channels:
-    #     message = await client.send_message(channel, '*No RSVPs*')
-    #     rsvp_message = RSVPMessage(message)
-    #     rsvp_messages[channel] = rsvp_message
-    #     await client.pin_message(message)
 
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-    if message.content.startswith('!rsvp_setup') and message.channel not in rsvp_messages.keys():
-        rsvp_message = await client.send_message(message.channel, '*No RSVPs*')
-        rsvp_messages[message.channel] = RSVPMessage(rsvp_message)
+@client.command(name='rsvp_setup', pass_context=True)
+async def rsvp_setup(context):
+    if context.message.channel not in rsvp_messages.keys() and context.message.author.server_permissions.administrator:
+        rsvp_message = await client.say('*No RSVPs*')
+        rsvp_messages[context.message.channel] = RSVPMessage(rsvp_message)
         await client.pin_message(rsvp_message)
 
-    if message.content.startswith('!rsvp_destroy'):
-        try:
-            rsvp_message = rsvp_messages.pop(message.channel)
-        except KeyError:
-            return
-        else:
-            for alert in rsvp_message.alerts:
-                await client.clear_reactions(alert.message)
-            await client.delete_message(rsvp_message.message)
+
+@client.command(name='rsvp_destroy', pass_context=True)
+async def rsvp_destroy(context):
+    if not context.message.author.server_permissions.administrator:
+        return
+    try:
+        rsvp_message = rsvp_messages.pop(context.message.channel)
+    except KeyError:
+        return
+    else:
+        for alert in rsvp_message.alerts:
+            await client.clear_reactions(alert.message)
+        await client.delete_message(rsvp_message.message)
 
 
 @client.event
@@ -75,10 +69,11 @@ async def on_reaction_remove(reaction, user):
 
     if reaction.emoji in RECOGNIZED_EMOJIS and client.user != reaction.message.author:
         alert = rsvp_message.get_alert(reaction.message)
-        alert.responses.delete(reaction, user)
-        rsvp_message.update_alerts()
-        rsvp_message.compose_content()
-        await client.edit_message(rsvp_message.message, rsvp_message.content)
+        if alert:
+            alert.responses.delete(reaction, user)
+            rsvp_message.update_alerts()
+            rsvp_message.compose_content()
+            await client.edit_message(rsvp_message.message, rsvp_message.content)
 
 
 @client.event
