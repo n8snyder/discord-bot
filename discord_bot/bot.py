3b711e8da5b8a6f68ba8bbee38952f9f830f4317
Projects <projects@Nates-MacBook-Pro.local>
2018-07-18 23:38:15 -0700

    Added expiration to alerts. Instead of removing all reactions to old messages, only remove the alert from rsvp message. Alerts can't be created from old messages.

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -26,7 +26,8 @@ async def on_ready():
 
 @client.command(name='rsvp_setup', pass_context=True)
 async def rsvp_setup(context):
-    if context.message.channel not in rsvp_messages.keys() and context.message.author.server_permissions.administrator:
+    if (context.message.channel not in rsvp_messages.keys() and
+            context.message.author.server_permissions.administrator):
         rsvp_message = await client.say('*No RSVPs*')
         rsvp_messages[context.message.channel] = RSVPMessage(rsvp_message)
         await client.pin_message(rsvp_message)
@@ -55,9 +56,10 @@ async def on_reaction_add(reaction, user):
 
     if reaction.emoji in RECOGNIZED_EMOJIS and client.user != reaction.message.author:
         alert = rsvp_message.create_alert(reaction.message)
-        alert.responses.post(reaction, user)
-        rsvp_message.compose_content()
-        await client.edit_message(rsvp_message.message, rsvp_message.content)
+        if alert:
+            alert.responses.post(reaction, user)
+            rsvp_message.compose_content()
+            await client.edit_message(rsvp_message.message, rsvp_message.content)
 
 
 @client.event
@@ -103,33 +105,22 @@ async def on_message_delete(message):
         rsvp_message.compose_content()
         await client.edit_message(rsvp_message.message, rsvp_message.content)
 
-# on_reaction_clear is the same as on_message_delete
 
-
-@client.event
-async def on_reaction_clear(message, reactions):
-    try:
-        rsvp_message = rsvp_messages[message.channel]
-    except KeyError:
-        return
-
-    if client.user != message.author:
-        rsvp_message.delete_alert(message)
-        rsvp_message.compose_content()
-        await client.edit_message(rsvp_message.message, rsvp_message.content)
-
-
-async def remove_reactions():
+async def remove_alerts():
     await client.wait_until_ready()
     while not client.is_closed:
-        await asyncio.sleep(60*30)
-        now = arrow.utcnow()
-        for rsvp_message in rsvp_messages.values():
-            for alert in rsvp_message.alerts:
-                if (now - arrow.get(alert.message.timestamp)).seconds > 60*60 + 60*45:
-                    await client.clear_reactions(alert.message)
-        print('Finished removing reactions.')
+        await asyncio.sleep(1)
+
+        if any([rsvp_message.alerts for rsvp_message in rsvp_messages.values()]):
+            print('Removing expired alerts...')
+            await asyncio.sleep(60 * 5)
+            for rsvp_message in rsvp_messages.values():
+                for alert in rsvp_message.alerts:
+                    if alert.is_expired:
+                        rsvp_message.delete_alert(alert.message)
+                rsvp_message.compose_content()
+                await client.edit_message(rsvp_message.message, rsvp_message.content)
 
 
-client.loop.create_task(remove_reactions())
+client.loop.create_task(remove_alerts())
 client.run(TOKEN)
--- discord_bot/utils.py
+++ discord_bot/utils.py
@@ -1,6 +1,7 @@
 
 from collections import defaultdict
 from discord.utils import find
+import arrow
 
 NUMBER_EMOJIS = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
 QUESTION_EMOJIS = ['❓', '❔']
@@ -10,10 +11,12 @@ EMOJI_TEXT = {'1⃣': '1', '2⃣': '2', '3⃣': '3', '4⃣': '4', '5⃣': '5',
 
 
 class RSVPMessage():
-    def __init__(self, message):
+    def __init__(self, message, expiration=60 * 60 + 60 * 45):
         self.message = message
         self.content = message.content
         self.alerts = []
+        # expiration is time in seconds until alerts become untracked.
+        self.expiration = expiration
 
     def get_alert(self, message):
         for alert in self.alerts:
@@ -27,8 +30,9 @@ class RSVPMessage():
 
     def create_alert(self, message):
         alert = self.get_alert(message)
-        if not alert and message.id != self.message.id:
-            alert = Alert(message)
+        if (not alert and message.id != self.message.id and
+                (arrow.utcnow() - arrow.get(message.timestamp)).seconds < self.expiration):
+            alert = Alert(message, self.expiration)
             self.alerts.append(alert)
         return alert
 
@@ -50,10 +54,16 @@ class RSVPMessage():
 
 
 class Alert():
-    def __init__(self, message):
+    def __init__(self, message, expiration=None):
         self.responses = Responses()
         self.message = message
         self.composed_content = message.content
+        self.post_date = message.timestamp
+        self.expiration = expiration
+
+    @property
+    def is_expired(self):
+        return (arrow.utcnow() - arrow.get(self.post_date)).seconds > self.expiration
 
     def update_message(self, message):
         self.message = message
